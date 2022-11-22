1956457764b068a191d28d96945842b20c40bdf9
Stef Keuken <91873549+Kitch41@users.noreply.github.com>
2022-11-22 12:01:10 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
-// JavaScript Document
-console.log("hi");
+// variabelen
+
+
+// nav button javascript
+var navbutton = document.querySelector("header section:nth-of-type(2) button");
+
+
+
+navbutton.addEventListener("click", navbuttonClick);
+
+
+// functions
+
+function navbuttonClick() {
+  navbutton.classList.toggle("menuOpen");
+}
